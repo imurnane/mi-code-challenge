196bedc280ea020bd3e0f7d8ccd60bf269af690a
Ian Murnane <imurnane@internode.on.net>
2021-03-01 14:08:30 +1000

    implemented basic react-chartsjs-2

--- frontend/src/component/visualiseResults.js
+++ frontend/src/component/visualiseResults.js
@@ -1,12 +1,36 @@
+import {Bar, Doughnut} from "react-chartjs-2";
+
 export default function VisualiseResults({ result }) {
   if (result === null) {
     return (
       <div>No result yet</div>
     );
   }
-  else return (
-    <div>
-      Tonight's lotto numbers are {JSON.stringify(result)}
-    </div>
-  );
+  else {
+    const data = {
+      labels: Object.keys(result.booking_distance_bins),
+      datasets: [{
+        label: 'My Dataset',
+        data: Object.values(result.booking_distance_bins),
+        backgroundColor: [ "rgb(2, 62, 138)", "rgb(0, 119, 182)", "rgb(0, 150, 199)", "rgb(0, 180, 216)" ],
+      }],
+    };
+
+    const options = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+          }
+        }]
+      }
+    };
+
+    return (
+      <div>
+        <Doughnut data={data} />
+        <Bar data={data} options={options} />
+      </div>
+    );
+  }
 }
