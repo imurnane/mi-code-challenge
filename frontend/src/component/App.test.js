e7e588c89245df595f9f6889c9b2d42f34b34725
Ian Murnane <imurnane@internode.on.net>
2021-03-03 01:58:34 +1000

    update frontend test

--- frontend/src/component/App.test.js
+++ frontend/src/component/App.test.js
@@ -1,3 +1,5 @@
+import React from "react";
+import { shallow } from 'enzyme';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -5,8 +7,17 @@ jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
    Map: () => ({})
 }));
 
-test('renders learn react link', () => {
+it('displays initial elements', () => {
   render(<App />);
-  const textElement = screen.getByText(/Code Challenge/i);
-  expect(textElement).toBeInTheDocument();
+  const title = screen.getByText(/Mobility Intelligence/i);
+  const button = screen.getByText(/Begin Simulation/i);
+  expect(title).toBeInTheDocument();
+  expect(button).toBeInTheDocument();
+});
+
+xit('calls fetchSimulation on click', () => {
+  const wrapper = shallow(<App />);
+  const button = wrapper.find('#button-fetch-simulation');
+  button.simulate('click');
+  expect(true).toBeTruthy();
 });
